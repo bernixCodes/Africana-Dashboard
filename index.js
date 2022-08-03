6398d406d185fe9f92bfe421f97b18b49b13d861
bernice <bernice@hubtel.com>
2022-08-03 14:28:44 +0000

    dark mode, light mode

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const menubtn = document.querySelector("#menu-btn");
 const closebtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("aside");
+const themeToggler = document.querySelector(".theme-toggler");
+
 
 menubtn.addEventListener("click", ()=>{
     sidebar.style.display = "block"
@@ -8,4 +10,8 @@ menubtn.addEventListener("click", ()=>{
 
 closebtn.addEventListener("click", ()=>{
     sidebar.style.display = "none"
+})
+
+themeToggler.addEventListener("click", ()=>{
+    document.body.classList.toggle('dark-theme-variables')
 })
